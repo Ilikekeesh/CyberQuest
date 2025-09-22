15526ac1248387157a8672e3be7a47586ba8d8be
WorseThanBetter <dereudav000@hsestudents.org>
2025-09-22 09:29:53 -0400

    Account system in javascript/login page
    Created a class and a rudimentary way to create it using the text boxes and a confirmation button in account.js and login.html.

--- account.js
+++ account.js
@@ -1,8 +1,34 @@
 class Account {
-    constructor(brand) {
-        this.carname = brand;
+    constructor(name,pass,mail="N/A") {
+        this.username = name;
+        this.password = pass;
+        this.email = mail;
+        this.points = 0;
     }
-    present() {
-        return 'I have a ' + this.carname;
+    checkPoints(check) { //Sees if the player's points are equal/greater than check, and returns true. false otherwise.
+        if (check >= this.points) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+const element = document.getElementById("confirmInfo");
+element.addEventListener("click", checkAccount);
+
+const element2 = document.getElementById("creationInfo");
+
+function checkAccount(){
+    var user= document.getElementById("username").value;
+    var pass= document.getElementById("password").value;
+    console.log(user);
+    if (user=="" || pass==""){
+        const newAccount = new Account(user,pass);
+        element2.textContent="Account info invalid.";
+        console.log("Account info invalid.");
+    } else {
+        element2.textContent="Account created!";
+        console.log("Account created!");
     }
 }
